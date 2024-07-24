bbb4142b8cb85667307c8725897c6152cdb4b126
Krisan Manoharan <krisan1010@hotmail.com>
2024-07-24 17:30:47 -0400

    Add return logic for PhotoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,9 +2,23 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-const PhotoListItem = () => {
+const PhotoListItem = ({photo}) => {
+  const { id, username, profile, imageSource, location } = photo;
   
+  return(
+    <div className="photo-list-item" key={id}>
+      <img src={imageSource} alt={`${location.city}, ${location.country}`} className="photo" />
+      <div className="photo-info">
+        <img src={profile} alt={`${username}'s profile`} className="profile-picture" />
+        <div className="details">
+          <p className="username">{username}</p>
+          <p className="location">{`${location.city}, ${location.country}`}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
+
 
 
 export default PhotoListItem;
