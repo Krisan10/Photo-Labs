1bb00e104bd44b5ca2269212d73efd1fc6defd3b
Krisan Manoharan <krisan1010@hotmail.com>
2024-07-24 17:26:34 -0400

    Add prop to PhotoListItem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,6 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-//Data of profile
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   //Data of profile
@@ -32,7 +20,8 @@ const sampleDataForPhotoListItem = {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <PhotoListItem/>
+      <PhotoListItem photo = {sampleDataForPhotoListItem} /> 
+      {/* Using the name photo as a sub for prop */}
     </div>
   );
 };
