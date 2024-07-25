4bfa7f5b90807d2787bd6825bb9ecb9874273dda
Krisan Manoharan <krisan1010@hotmail.com>
2024-07-24 20:19:43 -0400

    Change className to fit BEM conventions.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,14 +5,14 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = ({photo}) => {
   const { id, username, profile, imageSource, location } = photo;
   
-  return(
-    <div className="photo-list-item" key={id}>
-      <img src={imageSource} alt={`${location.city}, ${location.country}`} className="photo" />
-      <div className="photo-info">
-        <img src={profile} alt={`${username}'s profile`} className="profile-picture" />
-        <div className="details">
-          <p className="username">{username}</p>
-          <p className="location">{`${location.city}, ${location.country}`}</p>
+  return (
+    <div className="photo-list__item" key={id}>
+      <img src={imageSource} alt={`${location.city}, ${location.country}`} className="photo-list__image" />
+      <div className="photo-list__user-details">
+        <img src={profile} alt={`${username}'s profile`} className="photo-list__user-profile" />
+        <div className="photo-list__user-info">
+          <p className="photo-list__username">{username}</p>
+          <p className="photo-list__user-location">{`${location.city}, ${location.country}`}</p>
         </div>
       </div>
     </div>
