31a1cf9f915cf615c9eccf55483850507075f967
Krisan Manoharan <krisan1010@hotmail.com>
2024-07-26 18:11:23 -0400

    Import mock photos and topics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
+import topics from 'mocks/topics';
+import photos from 'mocks/photos';
 
 // const photos = new Array(3).fill(sampleDataForPhotoListItem);
 // const photoListItems = photos.map((photo, index) => 
 //   <PhotoListItem key={index} photo={photo} />
 
-
 const App = () => {
   return (
     <div className="App">
-      <HomeRoute />
-
+      <HomeRoute photos={photos} topics={topics} />
     </div>
   );
 };
