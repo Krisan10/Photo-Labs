8eb15907767c6030ac27b8f7c450c25e2f674f21
Krisan Manoharan <krisan1010@hotmail.com>
2024-07-26 14:38:15 -0400

    Change const for photo and add user and urls for clear code.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import PhotoFavButton from './PhotoFavButton';
-
 import "../styles/PhotoListItem.scss";
 
-const PhotoListItem = ({photo}) => {
-  const { id, username, profile, imageSource, location } = photo;
-  
+const PhotoListItem = ({ photo }) => {
+  const { id, user, urls, location } = photo;
+  const { username, profile, name } = user;
+  const { regular: imageSource } = urls;
+
   return (
     <div className="photo-list__item" key={id}>
       <div className="photo-list-item__fav-button">
@@ -15,14 +16,12 @@ const PhotoListItem = ({photo}) => {
       <div className="photo-list__user-details">
         <img src={profile} alt={`${username}'s profile`} className="photo-list__user-profile" />
         <div className="photo-list__user-info">
-          <p className="photo-list__username">{username}</p>
+          <p className="photo-list__name">{name}</p>
           <p className="photo-list__user-location">{`${location.city}, ${location.country}`}</p>
         </div>
       </div>
     </div>
   );
 };
-
-
 
 export default PhotoListItem;
