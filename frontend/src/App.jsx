0d45b856473f9b374ead6c4fe00348a66880e1af
Krisan Manoharan <krisan1010@hotmail.com>
2024-07-26 15:07:03 -0400

    Remove TopicList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.scss';
 import PhotoList from 'components/PhotoList';
 import TopicList from 'components/TopicList';
+import TopNavigation from 'components/TopNavigationBar';
 
 // const photos = new Array(3).fill(sampleDataForPhotoListItem);
 // const photoListItems = photos.map((photo, index) => 
@@ -11,8 +12,8 @@ import TopicList from 'components/TopicList';
 const App = () => {
   return (
     <div className="App">
+      <TopNavigation />  
       <h1>Hello World</h1>
-      <TopicList />
       <PhotoList />
     </div>
   );
