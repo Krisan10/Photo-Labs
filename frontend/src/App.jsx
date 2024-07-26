5d69ab3aa726f31b8d0cc1a8e9bf90bbbf74a42a
Krisan Manoharan <krisan1010@hotmail.com>
2024-07-26 14:52:33 -0400

    Add topicList to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
-
 import './App.scss';
 import PhotoList from 'components/PhotoList';
-
-// Note: Rendering a single component to build components in isolation
-const App = () => {
-  //Present the photo three times
+import TopicList from 'components/TopicList';
 
 // const photos = new Array(3).fill(sampleDataForPhotoListItem);
 // const photoListItems = photos.map((photo, index) => 
 //   <PhotoListItem key={index} photo={photo} />
 
-return (
-  <div className="App">
-    <h1>Hello World</h1>
-    <PhotoList />
-  </div>
-);
-};
 
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <TopicList />
+      <PhotoList />
+    </div>
+  );
+};
 
 export default App;
