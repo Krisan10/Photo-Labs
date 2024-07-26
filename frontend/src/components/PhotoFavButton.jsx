7b999822c489724677559d2d69291f65c18298a8
Krisan Manoharan <krisan1010@hotmail.com>
2024-07-26 10:58:25 -0400

    Add favourite button and toggle

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,13 +4,20 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [favourite, setFavourite] = useState(false);
+// handleClick toggles the favourite state between true and false
+  const handleClick = () => {
+    setFavourite(!favourite);
+  };
+
   return (
-    <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
-      </div>
-    </div>
+    // Button to add a click feature to manage the toggle of favourite.
+    <button onClick={handleClick} className="photo-list__fav-button"> 
+    <FavIcon selected={favourite} displayAlert={favourite} />
+
+  </button>
   );
 }
+
 
 export default PhotoFavButton;
