ae12c2e780cb6f7362260f12ae52a6c0e3dcc571
Krisan Manoharan <krisan1010@hotmail.com>
2024-07-26 10:58:45 -0400

    Add photoFavButton to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
+import PhotoFavButton from 'components/PhotoFavButton';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
@@ -21,14 +22,17 @@ const sampleDataForPhotoListItem = {
 const photos = new Array(3).fill(sampleDataForPhotoListItem);
 const photoListItems = photos.map((photo, index) => 
   <PhotoListItem key={index} photo={photo} />
+  
 );
 
 return (
   <div className="App">
     <h1>Hello World</h1>
     {photoListItems}
+    <PhotoFavButton />
   </div>
 );
 };
+
 
 export default App;
