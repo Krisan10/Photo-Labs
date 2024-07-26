3c6f8758e92644b32d48f6914ab3bfef3afacb99
Krisan Manoharan <krisan1010@hotmail.com>
2024-07-26 18:12:42 -0400

    HomeRoute accepts props

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = () => {
+const HomeRoute = ({ photos, topics }) => {
   return (
     <div className="home-route">
-      <TopNavigation />  
+      <TopNavigation topics={topics} />  
       <h1>Hello World</h1>
-      <PhotoList />
+      <PhotoList photos={photos} />
     </div>
   );
 };
