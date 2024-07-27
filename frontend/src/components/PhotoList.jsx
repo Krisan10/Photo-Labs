6a68b4700d111b6101ea237ac317bdea8534434c
Krisan Manoharan <krisan1010@hotmail.com>
2024-07-27 09:17:19 -0400

    Add favoutirePhotos and toggleFavourite as parameters and add them to the code.

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import PhotoListItem from "./PhotoListItem";
 import "../styles/PhotoList.scss";
 
-const PhotoList = ({ photos }) => {
+const PhotoList = ({ photos, favouritePhotos, toggleFavourite }) => {
   return (
     <ul className="photo-list">
       {photos.map(photo => (
         <PhotoListItem
           key={photo.id}
           photo={photo}
+          isFavourite={!!favouritePhotos[photo.id]}
+          toggleFavourite={toggleFavourite}
         />
       ))}
     </ul>
