965b0c58ae88a43335febcd1452dd84befe46a77
Krisan Manoharan <krisan1010@hotmail.com>
2024-07-27 09:17:34 -0400

    Fix grammer issues

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,14 +3,14 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton( {isfavourite, onClick}) {
+function PhotoFavButton( {isFavourite, onClick}) {
   const [favourite, setFavourite] = useState(false);
 
 
   return (
     // Button to add a click feature to manage the toggle of favourite.
     <button onClick={onClick} className="photo-list__fav-button"> 
-     <FavIcon selected={isfavourite} displayAlert={isfavourite} />
+     <FavIcon selected={isFavourite} displayAlert={isFavourite} />
 
   </button>
   );
