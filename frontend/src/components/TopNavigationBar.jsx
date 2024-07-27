443f99d90bbee2ebda1ae17f3cfa2e34784dc75f
Krisan Manoharan <krisan1010@hotmail.com>
2024-07-27 09:55:33 -0400

    Add function to allow hasFavourites to check if a picture has been liked.

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -3,12 +3,18 @@ import '../styles/TopNavigationBar.scss';
 import FavBadge from './FavBadge';
 import TopicList from './TopicList';
 
-const TopNavigation = ({ topics }) => {
+const TopNavigation = ({ topics, favouritePhotos }) => {
+  /* Returns an array of values from favouritePhotos
+     .some() checks if one of those values is true
+     isFavourite returns the value of isFavourite  
+  */
+  const hasFavourites = Object.values(favouritePhotos).some(isFavourite => isFavourite);
+
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics={topics} />
-      <FavBadge />
+      {hasFavourites && <FavBadge />}
     </div>
   );
 };
