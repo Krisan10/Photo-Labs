8760bf387f6da46915a18e539a3c3d61556a99ca
Krisan Manoharan <krisan1010@hotmail.com>
2024-07-27 09:18:30 -0400

    Create useStare for favouritePhotos

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = ({ photos, topics }) => {
+  
+  const [favouritePhotos, setFavouritePhotos] = useState({});
+
+  
+  const toggleFavourite = useCallback((photoId) => {
+    setFavouritePhotos((prevFavourites) => ({
+      ...prevFavourites,
+      [photoId]: !prevFavourites[photoId]
+    }));
+  }, []);
+
   return (
     <div className="home-route">
       <TopNavigation topics={topics} />  
       <h1>Hello World</h1>
-      <PhotoList photos={photos} />
+      <PhotoList 
+        photos={photos} 
+        favouritePhotos={favouritePhotos}
+        toggleFavourite={toggleFavourite}
+      />
     </div>
   );
 };
