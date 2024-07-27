47868a5d2ec68cd60eb2af5a8a3a4428dcd47159
Krisan Manoharan <krisan1010@hotmail.com>
2024-07-27 14:00:37 -0400

    Add photo details for modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -4,6 +4,8 @@ import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = ({ closeDisplayModal, photo }) => {
   console.log('Selected Photo', photo);
+  if (!photo) return null;
+
   return (
     <div className="photo-details-modal">
       <button 
@@ -12,6 +14,13 @@ const PhotoDetailsModal = ({ closeDisplayModal, photo }) => {
       >
         <img src={closeSymbol} alt="close symbol" />
       </button>
+      <div className="photo-details-modal__content">
+        <img src={photo.urls.regular} alt={`Photo by ${photo.user.username}`} className="photo-details-modal__image" />
+        <div className="photo-details-modal__photographer-profile">
+          <p className="photo-details-modal__photographer-info">{photo.user.name}</p>
+          <p className="photo-details-modal__photographer-location">{`${photo.location.city}, ${photo.location.country}`}</p>
+        </div>
+        </div>
     </div>
   );
 };
