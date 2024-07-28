e537f490430e8a30f69811b589d72c2c54c10520
Krisan Manoharan <krisan1010@hotmail.com>
2024-07-27 21:26:55 -0400

    Added onClick for topics

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
 
-const TopicList = ({ topics }) => {
+const TopicList = ({ topics, onTopicClick }) => {
   return (
     <div className="top-nav-bar__topic-list">
       {topics.map(topic => (
         <TopicListItem
           key={topic.id}
           topic={topic}
+          onClick={onTopicClick}
         />
       ))}
     </div>
