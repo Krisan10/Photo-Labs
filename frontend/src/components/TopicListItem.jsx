c05ecbd0993996e7d7fb260a1686aac4fada7fd4
Krisan Manoharan <krisan1010@hotmail.com>
2024-07-27 21:27:07 -0400

    Added onClick for topics

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "../styles/TopicListItem.scss";
 
-const TopicListItem = ({ topic }) => {
+const TopicListItem = ({ topic, onClick }) => {
   const { id, title } = topic; // Destructure properties from topic object
 
   return (
-    <div className="topic-list__item">
+    <div className="topic-list__item" onClick={() => onClick(topic)}>
       <div className="topic-list__title">{title}</div>
     </div>
   );
