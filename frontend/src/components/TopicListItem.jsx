b84ca5aff03ea9495fbd6245eecfeb7d331c28c9
Krisan Manoharan <krisan1010@hotmail.com>
2024-07-31 21:40:46 -0400

    Fix spelling mistakes

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -3,7 +3,7 @@ import "../styles/TopicListItem.scss";
 
 const TopicListItem = ({ topic, onClick }) => {
   const { id, title } = topic; // Destructure properties from topic object
-
+  
   return (
     <div className="topic-list__item" onClick={() => onClick(topic)}>
       <div className="topic-list__title">{title}</div>
